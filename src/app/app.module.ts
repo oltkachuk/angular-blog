606e60e1f884fb076a73ca0df5b580457fceeb24
Oleh Tkachuk <olezuktkachuk22@gmial.com>
2020-11-16 18:44:01 +0200

    added router component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostComponent } from './shared/components/post/post.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    PostPageComponent,
+    HomePageComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
